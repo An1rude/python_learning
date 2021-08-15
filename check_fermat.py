ab70d7cd45e32178a61af50883c20d1e526f4a5f
An1rude <anirudhkumardash@gmail.com>
2021-08-15 21:12:29 +0530

    My hello world

---
+++ check_fermat.py
@@ -0,0 +1,25 @@
+print("Fermat's Last Theorem says that there are no positive integers a, b and c such that : ")
+print("a^n + b^n = c^n for all n > 2")
+print("Pick any 4 positive integers a,b,c and n to check.")
+
+def check_fermat():
+    a = input("a=")
+    b = input("b=")
+    c = input("c=")
+    n = input("n=")
+    if isinstance(a, int) and isinstance(b,int) and isinstance(c,int) and isinstance(n,int):
+         if n < 3:
+            print("n must be greater than 2")
+            n = int(input("n=")) 
+         if a**n + b**n == c**n and n > 2:
+            print("Holly smokes, Fermat was wrong")
+         if a**n + b**n != c**n and n > 2:
+            print(a**n + b**n, "is not equal to",  c**n)
+            print("Fermat is correct afterall")  
+    else:
+        print("Please note a, b, c and n have to be positive integers")
+        check_fermat()
+
+    
+
+check_fermat()
